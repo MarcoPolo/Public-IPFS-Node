43013d0779bd8c52f1097587759daaf9ae8fac6e
Marco Munizaga <git@marcopolo.io>
2022-10-19 00:15:17 +0100

    It know itself

--- workshop-server/main.go
+++ workshop-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"os"
 	"os/signal"
@@ -9,15 +10,56 @@ import (
 
 	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 	"github.com/multiformats/go-multiaddr"
 )
 
 const topicNameChat = "chat"
 const topicNameFiles = "files"
+const topicNameAddresses = "addresses"
+const peerIdFile = "/var/bootstrap/keys"
+
+func addrFactory(in []multiaddr.Multiaddr) []multiaddr.Multiaddr {
+	var out []multiaddr.Multiaddr
+
+	for _, a := range in {
+		ip4Val, err := a.ValueForProtocol(multiaddr.P_IP4)
+		if err == nil && ip4Val == "172.31.6.56" {
+			_, rest := multiaddr.SplitFirst(a)
+			m := multiaddr.Join(multiaddr.StringCast("/ip4/18.237.216.248"), rest)
+			out = append(out, m)
+		} else {
+			out = append(out, a)
+		}
+	}
+	return out
+}
 
 func main() {
-	h, err := libp2p.New(libp2p.ListenAddrs(multiaddr.StringCast("/ip4/0.0.0.0/tcp/7654")))
+	keyBytes, err := os.ReadFile(peerIdFile)
+	var key crypto.PrivKey
+
+	if err != nil {
+		fmt.Println("Missing keys", err)
+
+		key, _, err = crypto.GenerateEd25519Key(rand.Reader)
+		if err != nil {
+			panic(err)
+		}
+		kb, err := crypto.MarshalPrivateKey(key)
+		if err != nil {
+			panic(err)
+		}
+		os.WriteFile(peerIdFile, kb, 0700)
+	} else {
+		key, err = crypto.UnmarshalPrivateKey(keyBytes)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	h, err := libp2p.New(libp2p.ListenAddrs(multiaddr.StringCast("/ip4/0.0.0.0/tcp/7654")), libp2p.Identity(key), libp2p.AddrsFactory(addrFactory))
 	if err != nil {
 		panic(err)
 	}
@@ -53,9 +95,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	topicAddresses, err := ps.Join(topicNameAddresses)
+	if err != nil {
+		panic(err)
+	}
 
 	go followTopic(ctx, topicFiles)
 	go followTopic(ctx, topicChat)
+	go followTopic(ctx, topicAddresses)
 
 	go func() {
 		sub, err := topicFiles.Subscribe()
